4019c59faa524087de95eb9c096fca9bcdd20a2b
Hassan Kashif <rhkashif@gmail.com>
2024-01-10 16:04:53 +0000

    added read_dataset function

--- task_1_pipeline.py
+++ task_1_pipeline.py
@@ -7,3 +7,17 @@ This script reads the datasets, processes them according to the specified requir
 
 Author: Hassan Kashif
 """
+
+import pandas as pd
+
+def read_dataset(csv_path: str) -> DataFrame:
+    """
+    Reads a CSV file into a pandas DataFrame.
+
+    Args:
+    csv_path (str): The path to the CSV file
+
+    Returns:
+    DataFrame - A pandas DataFrame containing data from the CSV file
+    """
+    return pd.read_csv(file_path)
