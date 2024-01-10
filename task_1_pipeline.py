eb40a7755d37b081e5df0d83673e1b88c0ef8581
Hassan Kashif <rhkashif@gmail.com>
2024-01-10 16:13:44 +0000

    added main block and checked csv is loaded correctlty

--- task_1_pipeline.py
+++ task_1_pipeline.py
@@ -9,15 +9,26 @@ Author: Hassan Kashif
 """
 
 import pandas as pd
+from pandas import DataFrame
 
 def read_dataset(csv_path: str) -> DataFrame:
     """
     Reads a CSV file into a pandas DataFrame.
 
     Args:
-    csv_path (str): The path to the CSV file
+        csv_path (str): The path to the CSV file
 
     Returns:
-    DataFrame - A pandas DataFrame containing data from the CSV file
+        DataFrame: A pandas DataFrame containing data from the CSV file
     """
-    return pd.read_csv(file_path)
+    return pd.read_csv(csv_path)
+
+def main():
+    """
+    Execute the script functions to process data in the provided CSV files
+    """
+    df1 = read_dataset('dataset1.csv')
+    print(df1)
+
+if __name__ == "__main__":
+    main()
