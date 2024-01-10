542748c62f67dd549fb97e819cf2832d788665d7
Hassan Kashif <rhkashif@gmail.com>
2024-01-10 16:02:18 +0000

    Added module docstring

---
+++ task_1_pipeline.py
@@ -0,0 +1,9 @@
+"""
+Assessment 1: Data Pipelines 
+
+Script for processing dataset1.csv and dataset2.csv as part of Data Engineering Technical Assessment
+
+This script reads the datasets, processes them according to the specified requirements, and outputs a single combined CSV file.
+
+Author: Hassan Kashif
+"""
